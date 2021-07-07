55d1a50ae07c02e4adb28a489269dd73a7e8287d
pankajvma <pankajvma@gmail.com>
2021-07-07 18:31:07 +0530

    Updated Project Created ML API. Prediction feature set updated. Updated Notebook. Improved Model generation.

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def get_report(int_features):
 def home():
     return render_template('index.html', display_report = "none")
 
-@app.route('/',methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
--- model.py
+++ model.py
@@ -1,25 +1,58 @@
+import joblib
 import numpy as np
 import pandas as pd
 import pickle
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 
-df = pd.read_csv('model.csv')
+df = pd.read_csv('notebook/bank/bank-additional-full.csv')
 
-x = df.drop('subscribed',axis = 1).values
-y = df['subscribed'].values
+include = ['age', 'job', 'marital', 'education', 'default', 'housing', 'loan',
+           'contact', 'pdays', 'previous', 'poutcome', 'emp.var.rate',
+           'cons.price.idx', 'cons.conf.idx', 'euribor3m', 'nr.employed',
+           'subscribed']
 
-#We are training our model with all availabe data.
+df = df[include]
 
-abc = AdaBoostClassifier(n_estimators= 150, learning_rate= 1.0, algorithm= 'SAMME')
+# Data Preprocessing
+df['pdays'] = (df.pdays != 999).astype('uint8')
+df['subscribed'] = (df.subscribed == 'yes').astype('uint8')
+categoricals = []
+for col, col_type in df.dtypes.iteritems():
+    if col_type == 'O':
+        categoricals.append(col)
 
-#Fitting model with trainig data
+df_ohe = pd.get_dummies(df, columns=categoricals)
+
+df_ohe.to_csv('model.csv', index=False)
+df.to_json('model.json', orient='table', index=False)
+
+
+dependent_variable = 'subscribed'
+x = df_ohe[df_ohe.columns.difference([dependent_variable])]
+y = df_ohe[dependent_variable]
+
+#Oversampling the train data
+from imblearn.over_sampling import RandomOverSampler, SMOTE
+
+os=RandomOverSampler()
+x,y=os.fit_resample(x,y)
+
+
+abc = RandomForestClassifier(n_estimators=10)
+
 abc.fit(x, y)
 
-# Saving model to disk
-pickle.dump(abc, open('model.pkl','wb'))
+# Save your model
+joblib.dump(abc, 'model.pkl')
+print("Model dumped!")
 
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
+# Load the model that you just saved
+model = joblib.load('model.pkl')
+
+# Saving the data columns from training
+model_columns = list(x.columns)
+joblib.dump(model_columns, 'model_columns.pkl')
+print("Models columns dumped!")
 
 # test
-print(model.predict([[59.0,2,2343,1,0,1042.0,1.0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0]]))
+print(abc.predict(x[y==0].tail(20)))
